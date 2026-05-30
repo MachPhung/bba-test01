d87f3acea42488e1f7ee757844d7e0d649f277a9
Mach Phung <phung.mach2407@gmail.com>
2026-05-30 14:57:04 +0700

    feat: add solution for final challenge

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+for (let i = 1; i <= 100; i++) {
+  for (let j = i + 1; j <= 100; j++) {
+    if ((i + j) % 17 === 0) {
+      console.log(`(${i}, ${j})  là cặp số hợp lệ vì có tổng chia hết cho 17`);
+    }
+  }
+}
